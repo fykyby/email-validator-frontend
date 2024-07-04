324e0058900b4025055476b31539f86aeac258d5
fykyby <95149002+fykyby@users.noreply.github.com>
2024-07-04 15:52:55 +0200

    Put selected tab into url on authenticate page

--- src/routes/authenticate/+page.server.ts
+++ src/routes/authenticate/+page.server.ts
@@ -1,7 +1,14 @@
 import { redirect } from '@sveltejs/kit';
 
-export function load({ locals }) {
+export function load({ locals, url }) {
 	if (locals.user) {
 		redirect(303, '/app');
 	}
+
+	const tabParam = url.searchParams.get('tab');
+	const isTabParamValid = tabParam && (tabParam === 'login' || tabParam === 'register');
+
+	return {
+		currentTab: isTabParamValid ? tabParam : 'register'
+	};
 }
--- src/routes/authenticate/+page.svelte
+++ src/routes/authenticate/+page.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
-	import LogInForm from '$lib/components/LogInForm.svelte';
-	import RegisterForm from '$lib/components/RegisterForm.svelte';
-
+	import LogInForm from '$lib/components/auth/LogInForm.svelte';
+	import RegisterForm from '$lib/components/auth/RegisterForm.svelte';
 	import * as Tabs from '$lib/components/ui/tabs';
+	import { tick } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
+
+	type Props = {
+		data: {
+			currentTab: 'login' | 'register';
+		};
+	};
+
+	let { data }: Props = $props();
+
+	async function tabChanged() {
+		if (!browser) return;
+
+		await tick();
+		const searchParams = new URLSearchParams();
+		searchParams.set('tab', data.currentTab);
+		goto(`?${searchParams.toString()}`);
+	}
 </script>
 
 <h1 class="text-center">dutkovsky email validator</h1>
 
-<Tabs.Root value="register" class="mx-auto w-full max-w-lg">
+<Tabs.Root bind:value={data.currentTab} onValueChange={tabChanged} class="mx-auto w-full max-w-lg">
 	<Tabs.List class="w-full">
 		<Tabs.Trigger class="w-full" value="register">Register</Tabs.Trigger>
 		<Tabs.Trigger class="w-full" value="login">Log In</Tabs.Trigger>
